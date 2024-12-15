a9263d69b3b4a261ec9b407bd7a5c2b730b56e91
alswjdghks <jerrymin1221@naver.com>
2024-12-15 19:17:42 +0900

    fix : project setting & add .env file

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
 
+dotenv.config();
 const app = express();
 app.set('port',process.env.PORT || 3000);
+app.use('/',express.static(path.join(__dirname,'public')));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extends: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name:'session-cookie',
+}));
+
+app.use((req,res,next) => {
+    console.log('모든 요청에 다 실행됩니다.');
+});
+
 
 app.get('/',(req,res) => {
     res.send('Hello,express');
